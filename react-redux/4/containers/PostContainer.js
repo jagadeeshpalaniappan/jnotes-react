e931c617dcc8a2f77329ccca5408f5542ac3eaad
jagadeeshpalaniappan <jagtechgeek@gmail.com>
2020-05-03 16:55:27 -0700

    1 file modified, 4 added

---
+++ react-redux/4/containers/PostContainer.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { AddItemForm, List, ListItem } from "../../components";
+
+import {
+  addPostAction,
+  editPostAction,
+  deletePostAction
+} from "../redux/post.state";
+
+const PostContainer = ({ posts, addPost, editPost, deletePost }) => {
+  const handleAdd = name => {
+    console.log("AddPost:", name);
+    addPost({ name });
+  };
+
+  const handleEdit = (e, post) => {
+    console.log("EditPost:", post);
+    editPost(post);
+  };
+
+  const handleDelete = (e, post) => {
+    console.log("DeletePost:", post);
+    deletePost(post);
+  };
+
+  return (
+    <div>
+      <h3> PostContainer: </h3>
+      <AddItemForm onAdd={handleAdd} />
+
+      {posts && (
+        <List>
+          {posts.map(post => (
+            <ListItem item={post} onEdit={handleEdit} onDelete={handleDelete} />
+          ))}
+        </List>
+      )}
+
+      {!(posts && posts.length > 0) && "No posts found"}
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    posts: state.postState.posts
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    addPost: post => dispatch(addPostAction(post)),
+    editPost: post => dispatch(editPostAction(post)),
+    deletePost: post => dispatch(deletePostAction(post))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PostContainer);
---
+++ react-redux/4/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import { v4 as uuidv4 } from "uuid";
+
+import store from "./redux/app.store";
+import UserContainer from "./containers/UserContainer";
+import PostContainer from "./containers/PostContainer";
+
+import { AppHeader } from "../components";
+
+// Example: Blog App (BlogPosts, Users)
+
+//--------------------------------- React -----------------------------------
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div>
+        <AppHeader title="My App 4" />
+        <UserContainer />
+        <PostContainer />
+      </div>
+    </Provider>
+  );
+};
+render(<App />, document.getElementById("root"));
