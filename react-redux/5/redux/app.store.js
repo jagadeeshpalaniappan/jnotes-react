4a36c569064d3a7226351e82d6e1527f6b59833d
jagadeeshpalaniappan <jagtechgeek@gmail.com>
2020-05-05 19:37:26 -0700

    1 file modified

--- react-redux/5/redux/app.store.js
+++ react-redux/5/redux/app.store.js
@@ -1,9 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import undoable, { includeAction } from "redux-undo";
+import logger from "redux-logger";
 
 import { userReducer, UNDO_USER, REDO_USER } from "./user.state";
 import { postReducer } from "./post.state";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 //--------------------------------- Redux: Store -----------------------------------
 
 const UNDO_ALL = "UNDO_ALL";
@@ -11,12 +13,16 @@ const REDO_ALL = "REDO_ALL";
 
 const rootReducer = combineReducers({
   userState: undoable(userReducer, {
-    filter: includeAction([UNDO_ALL, REDO_ALL, UNDO_USER, REDO_USER])
+    undoType: "UNDO_USER",
+    redoType: "REDO_USER"
+    // filter: includeAction([UNDO_ALL, REDO_ALL, UNDO_USER, REDO_USER])
   }),
   postState: undoable(postReducer, {
-    filter: includeAction([UNDO_ALL, REDO_ALL])
+    // filter: includeAction([UNDO_ALL, REDO_ALL])
   })
 });
-const store = createStore(rootReducer);
+
+const middleware = [logger];
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
 
 export default store;
