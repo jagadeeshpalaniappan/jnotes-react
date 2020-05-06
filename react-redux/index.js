2d63189d617529a8061b1ecc5c7ef488aea62aa4
jagadeeshpalaniappan <jagtechgeek@gmail.com>
2020-05-05 22:16:05 -0700

    1 file modified, 4 added

--- react-redux/index.js
+++ react-redux/index.js
@@ -4,6 +4,8 @@ console.log(":: React Redux ::");
 // import './1';
 // import './2';
 // import './3';
-import './4';   // redux-logger, redux-devtools-extension
-// import './5';   // redux-undo, redux-logger, redux-devtools-extension
+// import './4';   // redux-logger, redux-devtools-extension
+// import './5';   // redux-undo
 // import './6';   // my-undo-impln
+
+import './7';   // my-undo-impln
