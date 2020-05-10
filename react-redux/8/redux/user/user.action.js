b426ae2b561adf2828bd42b1c0fb3de94f6cf242
jagadeeshpalaniappan <jagtechgeek@gmail.com>
2020-05-09 17:32:42 -0700

    1 file modified, 1 added, 1 deleted

--- react-redux/8/redux/user/user.action.js
+++ react-redux/8/redux/user/user.action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { apiGetUsers } from "./user.getAll.api.action.js";
+import { apiGetUsers } from "./user.getUsers.api.action";
 
 export const getUsers = () => {
   /* getUsers: can call any source (just keep it abstract)
