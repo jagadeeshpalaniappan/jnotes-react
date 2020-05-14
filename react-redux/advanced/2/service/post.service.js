64a14bda89eaf04f0511c46f9a2f4eac7e51d7bf
jagadeeshpalaniappan <jagtechgeek@gmail.com>
2020-05-13 18:19:58 -0700

    1 file modified

--- react-redux/advanced/2/service/post.service.js
+++ react-redux/advanced/2/service/post.service.js
@@ -1,3 +1,3 @@
 // export * from "./fetch/post.service.js"; // VERY-HARD to use graphql
-export * from "./graphql-request/post.service.js"; // EASY, lightweight,  // RECOMENDED (if we are using Redux)
-// export * from "./apollo/post.service.js"; // FEATURE-RICH (graphql caching / pagination / reactComponentIntegration) // using this we can avoid Redux itself
+// export * from "./graphql-request/post.service.js"; // EASY, lightweight,  // RECOMENDED (if we are using Redux)
+export * from "./apollo/post.service.js"; // FEATURE-RICH (graphql caching / pagination / reactComponentIntegration) // using this we can avoid Redux itself
--- react-redux/advanced/2/service/user.service.js
+++ react-redux/advanced/2/service/user.service.js
@@ -1,3 +1,3 @@
 // export * from "./fetch/user.service.js"; // VERY-HARD to use graphql
-export * from "./graphql-request/user.service.js"; // EASY, lightweight,  // RECOMENDED (if we are using Redux)
+// export * from "./graphql-request/user.service.js"; // EASY, lightweight,  // RECOMENDED (if we are using Redux)
 // export * from "./apollo/user.service.js"; // FEATURE-RICH (graphql caching / pagination / reactComponentIntegration) // using this we can avoid Redux itself
