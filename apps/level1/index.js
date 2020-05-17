b0c205432739c1ce1e50a6c7e61f29b1a9680253
jagadeeshpalaniappan <jagtechgeek@gmail.com>
2020-05-16 18:06:07 -0700

    cu

--- apps/level1/index.js
+++ apps/level1/index.js
@@ -1,4 +1,3 @@
-
 // Opinionated (Options)
 
 // -------------------------- (with Redux) ------------------------------
@@ -9,7 +8,7 @@ Components: React (Class Components)
 State Management: [ LocalState: Redux, RemoteState: Redux ]
 API: REST
 */
-import "./0-react-old-redux";   // OLD-SCHOOL
+import "./0-react-old-redux"; // OLD-SCHOOL
 
 /*
 #### OPTION-1 ####
@@ -21,19 +20,27 @@ import "./1-react-redux";
 
 // ------------------------ (without Redux) ------------------------------
 
+
 /*
 #### OPTION-2 ####
 Components: React (Functional Components - using Hooks)
-State Management: [ LocalState: Context API, RemoteState: Context API ]
+State Management: [ LocalState: Context API (Redux Pattern), RemoteState: Context API (Redux Pattern)]
 API: REST / GraphQL
 */
-import "./2-react-context-reduxpattern";
-
+import "./2-react-context-reduxpattern"; // BEST (during 2020)
 
 /*
 #### OPTION-3 ####
 Components: React (Functional Components - using Hooks)
-State Management: [ LocalState: Context API, RemoteState: Apollo Client ]
+State Management: [ LocalState: Context API (Redux Pattern), RemoteState: Apollo Client ]
 API: GraphQL
 */
-import "./3-react-context-apollo";
+import "./3-react-context-apollo"; // BEST (during 2020)
+
+/*
+#### OPTION-4 ####
+Components: React (Functional Components - using Hooks)
+State Management: [ LocalState: Apollo Client, RemoteState: Apollo Client ]
+API: GraphQL
+*/
+import "./4-react-apollo";
