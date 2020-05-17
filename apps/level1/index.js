c9344681816aaefe4fe3514549da9c5d96641479
jagadeeshpalaniappan <jagtechgeek@gmail.com>
2020-05-16 17:49:34 -0700

    4 files added

---
+++ apps/level1/index.js
@@ -0,0 +1,16 @@
+
+
+// React, Redux, (REST / GraphQL)
+// State Management: [ LocalState: Redux, RemoteState: Redux ]
+import './1-react-redux';
+
+// ------------------------ (without Redux) ------------------------------
+
+// React, Context API (Redux Pattern Only), (REST / GraphQL)
+// State Management: [ LocalState: Context API, RemoteState: Context API ]
+import './2-react-context-reduxpattern';
+
+
+// React, Context API (Redux Pattern Only), Apollo Client, GraphQL
+// State Management: [ LocalState: Context API, RemoteState: Apollo Client ]
+import './3-react-context-apollo';
