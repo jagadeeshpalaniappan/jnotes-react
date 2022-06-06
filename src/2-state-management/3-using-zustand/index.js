3086f8c4495b9f4fc835e517a1d0f21699509f2b
jagadeeshpalaniappan <jagadeeshthegeek@gmail.com>
2022-06-05 20:26:42 -0700

    4 files modified

--- src/2-state-management/3-using-zustand/index.js
+++ src/2-state-management/3-using-zustand/index.js
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom';
 // import App from './1-counter';
 // import App from './2-counter-todos';
 // import App from './3-counter-todos-useMemo'; // useMemo: avoids 'getVisibleTodos' unecessary exceution
-import App from './4-counter-todos-useMemo-perf'; // useMemo: avoids 'getVisibleTodos' unecessary exceution
+import App from './4-counter-todos-useMemo-perf';
+/* 
+instead of 'todos' array, spilt them into 'todoIds' and 'todoMap'
+  - updating an todo item does not re-render all the TodoListItem component
+*/
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 // LEARN: React Hooks
-// import './hooks/index';
+// import './1-hooks/index';
 
 // LEARN: Redux
 // import './x--more-redux/redux/index';
@@ -13,6 +13,6 @@ import './index.css';
 // import "./x--more-redux/react-redux/advanced/index";
 
 // LEARN: React Redux Pattern
-// import "./react-redux-pattern/with-redux/index"; // redux, reselect
-// import "./react-redux-pattern/without-redux/index"; // using: useReducer, useContext, useMemo, useCallback
-import './react-redux-pattern/using-zustand/index'; // using zustand, useMemo
+// import "./2-state-management/1-using-redux/index"; // redux, reselect
+// import "./2-state-management/2-using-react-context/index"; // using: useReducer, useContext, useMemo, useCallback
+import './2-state-management/3-using-zustand/index'; // using zustand, useMemo
